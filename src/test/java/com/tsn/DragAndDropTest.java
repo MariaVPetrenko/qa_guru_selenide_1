09fe8534a43bbb8883b0ebb5f7d4542171d8e0e4
MariaVPetrenko <petrenkomariav@gmail.com>
2022-02-26 16:34:08 +0700

    Added drag and drop test

---
+++ src/test/java/com/tsn/DragAndDropTest.java
@@ -0,0 +1,24 @@
+package com.tsn;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void DragAndDropTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
